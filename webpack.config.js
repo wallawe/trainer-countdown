30df41858e6b7de67c31ed9d24651847985bd3be
Your Name <wallac.will@gmail.com>
2016-08-04 21:52:24 -0400

    Initial commit - barebones app

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/app/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    entry: [
+        './app/index.js'
+    ],
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    },
+    output: {
+        filename: "index_bundle.js",
+        path: __dirname + '/dist'
+    },
+    plugins: [HTMLWebpackPluginConfig]
+}
